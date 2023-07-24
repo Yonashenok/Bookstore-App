25f05bfcba1cdbd19b0c3b19be1b8c5ecd238dbe
Yonashenok <yonashenok24@gmail.com>
2023-07-24 15:42:08 +0300

    feat: dispatch remove handler

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/booksSlice';
 import classes from './Book.module.css';
+/* eslint-disable react/prop-types */
 
-const Book = () => (
-  <li className={classes.listContainer}>
-    <div>
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
+  const handleRemoveBook = (id) => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <li className={classes.listContainer}>
       <div>
-        <p>Action</p>
-        <h2>The Hunger Game</h2>
-        <p>Suzanne Collins</p>
+        <div>
+          <p>{book.category}</p>
+          <h2>{book.title}</h2>
+          <p>{book.author}</p>
+        </div>
+        <div className={classes.actionBtn}>
+          <button type="button">Comments</button>
+          <button onClick={() => handleRemoveBook(book.item_id)} type="button">
+            Remove
+          </button>
+          <button type="button">Edit</button>
+        </div>
       </div>
-      <div className={classes.actionBtn}>
-        <button type="button">Comments</button>
-        <button type="button">Remove</button>
-        <button type="button">Edit</button>
+      <div>
+        <p>64%</p>
+        <p>Completed</p>
       </div>
-    </div>
-    <div>
-      <p>64%</p>
-      <p>Completed</p>
-    </div>
-    <div>
-      <p>Current Chapter</p>
-      <p>Chapter 17</p>
-      <button type="button">UPDATE PROGRESS</button>
-    </div>
-  </li>
-);
+      <div>
+        <p>Current Chapter</p>
+        <p>Chapter 17</p>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
+    </li>
+  );
+};
 
 export default Book;
