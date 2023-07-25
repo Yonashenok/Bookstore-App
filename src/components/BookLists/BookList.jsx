715ba5d256ff8c5f792b832816a3af68e7f82da9
Yonashenok <yonashenok24@gmail.com>
2023-07-25 11:30:43 +0300

    Feat: get all books on  page reload

--- src/components/BookLists/BookList.jsx
+++ src/components/BookLists/BookList.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import axios from 'axios';
 import classes from './BookList.module.css';
 import Book from '../Book/Book';
+import { fetchBooks } from '../../redux/books/booksSlice';
 
 const BookList = () => {
   const { books } = useSelector((state) => state.book);
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
   return (
     <ul className={classes.bookListContainer}>
       {books.map((book) => (
